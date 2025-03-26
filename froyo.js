489dbe78d0398bbaf078edee28f06f64ace2c67d
Kristen George <kmstrumpfer@yahoo.com>
2025-03-26 17:17:44 -0400

    finished?

--- froyo.js
+++ froyo.js
@@ -1,9 +1,9 @@
 
 const userInputString = prompt(
     "Please enter some froyo flavors.",
-    "vanilla, vanilla, vanilla, strawberry, coffee, coffee"
+    "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
   );
- const froyoString = userInputString.split(", ");
+ const froyoString = userInputString.split(",");
 // froyoString is all the flavors split into individual strings
 
 console.log(froyoString)
@@ -14,14 +14,20 @@ function countFlavors(froyoString){
     for (let i = 0; i < froyoString.length; i++) {
     const froyoObj = {};
     //If there is no key with that name, it will be created and the value will be set to 1
-        if(froyoObj[froyoString[i]] === undefined){froyoObj[froyoString[i]] = 1;
+        
+        if(froyoObj[froyoString[i]] === undefined)
+            {froyoObj[froyoString[i]] = 1;
         }
     //If that key is already created, it will add 1 to the value
-         else{froyoObj[froyoString[i]] += 1}
-    console.table(countFlavors(froyoString))
+         else{froyoObj[froyoString[i]] += 1;}
+    
 
     }
+    console.log(froyoObj);
+    console.table(countFlavors(froyoString));
 }
+
+
  
     
 
